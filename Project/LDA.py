1d706f299f0557b88c51b369c119c2453e765b9f
lorenzotara <lorenzotara7@gmail.com>
2017-11-21 16:04:09 +0100

    end of task1

--- Project/LDA.py
+++ Project/LDA.py
@@ -2,6 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from Project.helpers import *
 import numpy as np
 from sklearn.decomposition import NMF, LatentDirichletAllocation
+import json
 import collections
 from nltk import PorterStemmer
 from nltk import word_tokenize
@@ -31,14 +32,32 @@ is a tweet
 '''
 That are not a retweet - and are not from his staff -> iPhone: http://varianceexplained.org/r/trump-tweets/
 '''
+# TODO: quote http://varianceexplained.org/r/trump-tweets/
 condensed_text = []
+condensed_date = []
+condensed_retweet_count = []
+condensed_favorite_count = []
+condensed_id = []
 for x in condensed:
     temp = x[x.is_retweet == False]
     temp = temp[temp.source != "Twitter for iPhone"]
     condensed_text.append(temp.text.tolist())
+    condensed_date.append(temp.created_at.tolist())
+    condensed_retweet_count.append(temp.retweet_count.tolist())
+    condensed_favorite_count.append(temp.favorite_count.tolist())
+    condensed_id.append(temp.id_str.tolist())
 
 
-flat_list = [item for sublist in condensed_text for item in sublist]
+flat_list_text = [item for sublist in condensed_text for item in sublist]
+flat_list_date = [item for sublist in condensed_date for item in sublist]
+flat_list_retweet_count = [item for sublist in condensed_retweet_count for item in sublist]
+flat_list_favorite_count = [item for sublist in condensed_favorite_count for item in sublist]
+flat_list_id = [item for sublist in condensed_id for item in sublist]
+
+# Date, retweet_count, favorite_count
+flat_list_info = []
+for date, ret, fav, id_ in zip(flat_list_date, flat_list_retweet_count, flat_list_favorite_count, flat_list_id):
+    flat_list_info.append((date, ret, fav, id_))
 
 '''
 STEMMING
@@ -90,7 +109,7 @@ no_features = 1000
 
 # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words=stopwords)
-tf = tf_vectorizer.fit_transform(flat_list)
+tf = tf_vectorizer.fit_transform(flat_list_text)
 tf_feature_names = tf_vectorizer.get_feature_names()
 
 '''
@@ -206,6 +225,8 @@ scores_shows = []
 interviews = []
 scores_interviews = []
 
+'''In every topic list we append the text and the index of the tweet in order to get the info later'''
+
 for topic_idx, topic in enumerate(lda_H):
 
     # Taking the indices of the documents ordered by importance in the topic
@@ -221,31 +242,33 @@ for topic_idx, topic in enumerate(lda_H):
         # BUSINESS - score no less than 0.6
         if topic_idx == 1 or topic_idx == 5:
 
-            business.append(flat_list[doc_index])
+            business.append([flat_list_text[doc_index], doc_index])
             scores_business.append(score)
 
         # VARIOUS - score can be 0.5
         elif topic_idx == 4 or topic_idx == 6:
 
-            various.append(flat_list[doc_index])
+            various.append([flat_list_text[doc_index], doc_index])
             scores_various.append(score)
 
         # POLITICS - score no less than 0.6
         elif topic_idx == 0 or topic_idx == 2:
-            politics.append(flat_list[doc_index])
+            politics.append([flat_list_text[doc_index], doc_index])
             scores_politics.append(score)
 
         # CELEBRITY APPRENTICE AND SHOWS - score no less than 0.6
         elif topic_idx == 3:
 
-            shows.append(flat_list[doc_index])
+            shows.append([flat_list_text[doc_index], doc_index])
             scores_shows.append(score)
 
         # INTERVIEWS AND DEBATES - score no less than 0.6
         elif topic_idx == 7:
 
-            interviews.append(flat_list[doc_index])
+            interviews.append([flat_list_text[doc_index], doc_index])
             scores_interviews.append(score)
+
+
 
 '''PRINT BUSINESS TOPIC'''
 # scores_business_idx_ordered = np.argsort(scores_business)[::-1]
@@ -302,12 +325,12 @@ In Politics Topic hillary appears only 15 times, probably the topic detection di
 For this reason we will use a function to retrieve her tweets
 '''
 
-hillary_topic = get_hillary_tweets_16_17(all_data)
+hillary_topic, hillary_info = get_hillary_tweets_16_17(all_data)
 
 # Removing the tweets in politics that contain hillary, because we are going to add every tweet that refers to hillary
 # later and we don't want to have duplicates
 for tweet in list(politics):
-    if ('hillary' in tweet) or ('clinton' in tweet) or ('Hillary' in tweet) or ('Clinton' in tweet):
+    if ('hillary' in tweet[0]) or ('clinton' in tweet[0]) or ('Hillary' in tweet[0]) or ('Clinton' in tweet[0]):
         politics.remove(tweet)
 
 
@@ -325,7 +348,8 @@ foreign_politics_keywords = ['iran', 'Iran', 'Iraq', 'iraq', 'oil', 'Oil', 'nucl
                              'military spending', 'Pakistan', 'Bin Laden', 'N.A.T.O', 'Assad', 'Libya',
                              'Solders', 'Soldiers', 'soldiers' 'weapon', 'weapons', 'Al Qaeda', 'Putin', 'military',
                              'Mexico', 'Europe', 'South Korea', 'military', 'Canada', 'Mubarak', 'Islamists',
-                             'TERRORISTS', 'OIL', 'Afghan'] # + russia, china
+                             'TERRORISTS', 'OIL', 'Afghan'] + china_keywords
+
 internal_politics_keywords = ['Government', 'government', 'govt.', 'Washington', 'republicans', 'Republicans', 'Repubs',
                               'Democrats', 'democrats', 'tax', 'taxes', 'debt', 'jobs', 'job', 'borders', 'border',
                               'Wall Street', 'deficit', 'immigrants', 'employment', 'unemployment', 'deficits',
@@ -337,7 +361,8 @@ internal_politics_keywords = ['Government', 'government', 'govt.', 'Washington',
                               'candidate', 'Fed', 'Amendment', '#TedCruz', 'defense cuts', 'drug abuse',
                               'food stamps', 'Jimmy Carter', 'guns are outlawed', 'candidate', 'Parties', 'Reagan',
                               'Chafee', 'Insurance', 'civil liberties', 'Federal Court', 'Supreme Court', 'CIA',
-                              'lobbyists'] # + obama + hillary
+                              'lobbyists'] + obama_keywords #  + hillary
+
 trump_keywords = ['@realDonaldTrump', 'Trump', 'trump', 'Donald', 'Jeb', 'Bush', '#Trump',
                   'MAKE AMERICA GREAT AGAIN', 'Make America Great Again', '#TimeToGetTough', 'Trumpative',
                   '#DonaldTrump', 'TRUMP', 'I will', 'VICTORY', 'no other leader', 'DONALD', 'DONALD TRUMP',
@@ -353,57 +378,146 @@ foreign_politics_topic = []
 internal_politics_topic = []
 trump_4pre_topic = []
 not_got = []
+
+china_json = []
+obama_json = []
+foreign_politics_json = []
+internal_politics_json = []
+trump_4pre_json = []
+
 for tweet in politics:
 
     control = 0
 
+    date, ret, fav, id_ = flat_list_info[tweet[1]]
+
+    json_file = {
+        'created_at': str(date),
+        'id': id_,
+        'retweet_count': ret,
+        'favorite_count': fav,
+        'text': tweet[0],
+        'topic': ''
+    }
+
     for keyword in china_keywords:
-        if keyword in tweet:
+        if keyword in tweet[0]:
             china_topic.append(tweet)
+
+            json_file['topic'] = 'china'
+            china_json.append(json_file.copy())
+
             control += 1
             break
 
     for keyword in obama_keywords:
-        if keyword in tweet:
+        if keyword in tweet[0]:
             obama_topic.append(tweet)
+
+            json_file['topic'] = 'obama'
+            obama_json.append(json_file.copy())
+
             control += 1
             break
 
     for keyword in foreign_politics_keywords:
-        if keyword in tweet:
+        if keyword in tweet[0]:
             foreign_politics_topic.append(tweet)
+
+            json_file['topic'] = 'foreign_politics'
+            foreign_politics_json.append(json_file.copy())
+            # foreign_politics_json.append(copy.deepcopy(json))
+
             control += 1
             break
 
     for keyword in internal_politics_keywords:
-        if keyword in tweet:
+        if keyword in tweet[0]:
             internal_politics_topic.append(tweet)
+
+            json_file['topic'] = 'internal_politics'
+            internal_politics_json.append(json_file.copy())
+
             control += 1
             break
 
     for keyword in trump_keywords:
-        if keyword in tweet:
+        if keyword in tweet[0]:
             trump_4pre_topic.append(tweet)
+
+            json_file['topic'] = 'trump_politcs'
+            trump_4pre_json.append(json_file.copy())
+
             control += 1
             break
 
     if control == 0:
         not_got.append(tweet)
 
-politics += hillary_topic
+
+
+
+
+'''
+TO JSON
+'''
+business_json = []
+various_json = []
+shows_json = []
+interviews_json = []
+hillary_json = []
+
+
+topic_to_json(business, business_json, flat_list_info, 'business')
+topic_to_json(various, various_json, flat_list_info, 'various')
+topic_to_json(shows, shows_json, flat_list_info, 'shows')
+topic_to_json(interviews, interviews_json, flat_list_info, 'interviews_debates')
+topic_to_json_hillary(hillary_topic, hillary_json, hillary_info)
+topic_to_json_hillary(hillary_topic, internal_politics_json, hillary_info, internal=True) # internal + hillary
+
+with open('topic_tweets/business.json', 'w') as fp:
+    json.dump(business_json, fp)
+
+with open('topic_tweets/various.json', 'w') as fp:
+    json.dump(various_json, fp)
+
+with open('topic_tweets/shows.json', 'w') as fp:
+    json.dump(shows_json, fp)
+
+with open('topic_tweets/interviews.json', 'w') as fp:
+    json.dump(interviews_json, fp)
+
+with open('topic_tweets/hillary.json', 'w') as fp:
+    json.dump(hillary_json, fp)
+
+with open('topic_tweets/china.json', 'w') as fp:
+    json.dump(china_json, fp)
+
+with open('topic_tweets/obama.json', 'w') as fp:
+    json.dump(obama_json, fp)
+
+with open('topic_tweets/foreign_politics.json', 'w') as fp:
+    json.dump(foreign_politics_json, fp)
+
+with open('topic_tweets/internal_politics.json', 'w') as fp:
+    json.dump(internal_politics_json, fp)
+
+
+
+
 
 '''
 That's how we we will count the most common word in a topic
 '''
 #TODO: add a second stopword file for punctuation
-words_in_topic = []
-for tweet in china_topic:
-    words = word_tokenize(tweet)
-    for word in list(words):
-        if word in stopwords:
-            words.remove(word)
-    words_in_topic += words
-
-counter = collections.Counter(words_in_topic)
-print(counter.most_common())
+# words_in_topic = []
+# for tweet in china_topic:
+#     words = word_tokenize(tweet)
+#     for word in list(words):
+#         if word in stopwords:
+#             words.remove(word)
+#     words_in_topic += words
+#
+# counter = collections.Counter(words_in_topic)
+# print(counter.most_common())
 
