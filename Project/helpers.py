5f8d0724491c28d5cd4ac1eeb0a62f6297580ff4
lorenzotara <lorenzotara7@gmail.com>
2017-11-28 19:25:00 +0100

    Fixed structure and notebook - fake_news_notebook to delete

--- Project/helpers.py
+++ Project/helpers.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import os
 import glob
-import nltk
-from nltk.stem import WordNetLemmatizer
 import json
-from scipy.sparse import csr_matrix
-from scipy.sparse.linalg import svds
-import math
+import requests
+from bs4 import BeautifulSoup
 
 
 def load_lexicons():
+    """
+    Loading the 'emotions' and return a df with a unique column a word and 10 other columns of emotions
+    referred to that term
+    """
 
     lexicons = pd.read_table('lexicons/NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt', sep='\t',
                              names=('term', 'emotion', 'value'))
@@ -50,6 +51,35 @@ def load_data():
             condensed.append(file)
 
     return all_data, condensed, master
+
+
+def get_stopwords_from_html():
+    """
+    Saving stopwords from url
+    """
+
+    URL_sw = 'https://www.ranks.nl/stopwords'
+
+    r = requests.get(URL_sw, verify=False)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    div = soup.find(
+        lambda tag: tag.name == 'div' and tag.has_attr('id') and tag['id'] == "article09e9cfa21e73da42e8e88ea97bc0a432")
+    table = div.find(lambda tag: tag.name == 'table')
+
+    stopwords = []
+
+    cols = table.findAll('td')
+    for col in cols:
+        col = str(col)
+        col = col.replace('<td style="width: 33%;" valign="top">', '')
+        col = col.replace('<td valign="top">', '')
+        words = col.split("<br/>")
+        stopwords.extend(words)
+
+    with open('stopwords/stopwords_2.json', 'w') as fp:
+        json.dump(stopwords, fp)
 
 
 def select_time_interval(df, date_column, start_datetime, end_datetime):
@@ -151,104 +181,4 @@ def topic_to_json_hillary(hillary_topic, hillary_json, hillary_info, internal=Fa
             }
 
         hillary_json.append(json)
-
-
-def create_term_document_matrix(terms, documents):
-    """
-    Constructs a sparse matrix which contains n at r,c if term r is in document c n times
-    (rows -> terms, columns -> documents)
-    """
-    # Inversed index for faster lookup
-    terms_inv = {term: index for index, term in enumerate(terms)}
-
-    columns, rows, counts = zip(*[(doc_id, terms_inv[word], doc.count(word))
-                                  for doc_id, doc in enumerate(documents)
-                                  for word in filter(lambda w: len(w) > 0, doc.split(' '))])
-
-    return csr_matrix((counts, (rows, columns)), shape=(len(terms), len(documents)))
-
-
-def compute_term_frequency(term_index, doc_index, term_document_mat):
-    """
-    Computes the term frequency of term in doc using:
-    tf(term, doc) = doc.count(term) / max over terms of (doc.count(term))
-    """
-    doc_col = term_document_mat.getcol(doc_index)
-    return doc_col[term_index].toarray()[0, 0] / doc_col.max()
-
-
-def compute_inverse_document_frequency(term_index, matrix):
-    """
-    Computes the inverse document frequency of term using:
-    idf(term) = log_2(# documents in which term occurs / # documents)
-    """
-    ni = matrix.getrow(term_index).count_nonzero()
-    return -math.log(ni / matrix.shape[1], 2)
-
-
-def compute_tfidf_matrix(term_document_mat):
-    """
-    Computes tfidf matrix: each element is tf(term, doc) * idf(term)
-    """
-    rows, cols = term_document_mat.nonzero()
-    tf = lambda t, d: compute_term_frequency(t, d, term_document_mat)
-    idf = lambda t: compute_inverse_document_frequency(t, term_document_mat)
-    vals = [tf(t, d) * idf(t) for t, d in zip(rows, cols)]
-    return csr_matrix((vals, (rows, cols)), shape=term_document_mat.shape)
-
-
-def get_all_terms(cleaned_tweets):
-    """
-    Extracts terms from tweets
-    """
-    all_terms = []
-    for tweet in cleaned_tweets:
-        for term in tweet.split(' '):
-            if len(term) > 0 and term not in all_terms:
-                all_terms.append(term)
-    return all_terms
-
-
-class Cleaner:
-    def __init__(self, punctuation, stop_words, lemmatizer=None, stemmer=None):
-        self._punctuation = set(punctuation)
-        self._lemmatizer = lemmatizer
-        self._stemmer = stemmer
-        self._stop_words = set(stop_words)
-
-    def clean_word(self, word):
-        if self._lemmatizer:
-            word = self._lemmatizer.lemmatize(word)
-        if self._stemmer:
-            word = self._stemmer.stem(word)
-
-        return word if word not in self._stop_words else ''
-
-    def clean_tweet(self, tweet):
-        char_cleaner = lambda char: char if char not in self._punctuation and char.isalpha() else ' '
-        tweet_clean = ''.join(map(char_cleaner, tweet.lower()))
-        cleaned_words = map(lambda w: self.clean_word(w), tweet_clean.split(' '))
-        long_words = filter(lambda w: len(w) > 1, cleaned_words)
-        return ' '.join(long_words)
-
-
-# if __name__ == '__main__':
-#
-#     punctuation = [',', '.', '-', ':', '\xa0', '%', '_', '!', '?', ';', '(', ')', '\n']
-#     with open('stopwords.json', 'r') as f:
-#         stopwords = json.load(f)
-#
-#     nltk.download('wordnet')
-#
-#     lemmatizer = WordNetLemmatizer()
-#     cleaner = Cleaner(punctuation, stopwords, lemmatizer)
-#
-#     with open('condensed_2009.json', 'r') as f:
-#         tweets = json.load(f)
-#
-#     tweets = list(map(lambda t: cleaner.clean_tweet(t['text']), tweets))
-#     terms = get_all_terms(tweets)
-#     td = create_term_document_matrix(terms, tweets)
-#     tfidf = compute_tfidf_matrix(td)
-#     print(tfidf)
 
